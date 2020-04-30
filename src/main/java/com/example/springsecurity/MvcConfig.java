ae67e1066e1b0c5dba2fb84ec164536c647c6573
YatheesanC <>
2020-04-30 23:19:41 +0530

    Initial Commit

---
+++ src/main/java/com/example/springsecurity/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.springsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/welcome").setViewName("welcome");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("login");
+        registry.addViewController("/error").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/accessdenied").setViewName("accessdenied");
+    }
+}
